2f5f3bc86b5a2c3836058295fc12bb21799b6995
Frantisekt <frankoba577@gmail.com>
2025-02-19 22:11:58 -0500

    agregando Cors; estado funcional guardar paquetes: ok

--- GlobeOnClick/src/main/java/com/backend/globeonclick/configuration/SecurityConfiguration.java
+++ GlobeOnClick/src/main/java/com/backend/globeonclick/configuration/SecurityConfiguration.java
@@ -11,6 +11,9 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -22,22 +25,28 @@ public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
+        http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                    //.requestMatchers("/auth/**").permitAll()
-                    //.requestMatchers("/users/me").authenticated()
-                    //.requestMatchers("/", "/index.html", "/login.html", "/register.html").permitAll()
-                    //.requestMatchers("/css/**", "/js/**").permitAll()
-                    //.requestMatchers("/users/**").hasRole("ADMIN")
-                    .anyRequest().permitAll()
-                    //.anyRequest().authenticated()
+                        .anyRequest().permitAll()
                 )
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider)
-                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
-                /*.oauth2ResourceServer(spec -> spec.jwt(Customizer.withDefaults()))*/
-                /*.httpBasic(Customizer.withDefaults())*/
-                .build();
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("http://localhost:5173");
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedHeader("*");
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/api/**", configuration);
+        return source;
     }
 }
