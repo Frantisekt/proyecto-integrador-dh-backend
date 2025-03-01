516fe6819936d8d4c272e0727fbc4d69d81b329b
Frantisekt <frankoba577@gmail.com>
2025-03-01 16:46:58 -0500

    agregando Servicios y controladores de Users

--- GlobeOnClick/src/main/java/com/backend/globeonclick/exception/GlobalExceptionHandler.java
+++ GlobeOnClick/src/main/java/com/backend/globeonclick/exception/GlobalExceptionHandler.java
@@ -1,9 +1,20 @@
-/*
+package com.backend.globeonclick.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
-    
+
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<?> handleResourceNotFoundException(ResourceNotFoundException ex) {
-        // Implementación
+    public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
-}*/
+
+    @ExceptionHandler(ResourceConflictException.class)
+    public ResponseEntity<String> handleResourceConflictException(ResourceConflictException ex) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
+    }
+}
