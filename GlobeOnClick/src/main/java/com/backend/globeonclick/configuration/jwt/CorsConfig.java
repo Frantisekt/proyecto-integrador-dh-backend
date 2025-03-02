b979e9d9102d298433def7d3a06af7f4263b291b
Frantisekt <frankoba577@gmail.com>
2025-03-01 19:44:49 -0500

    fix login connection

--- GlobeOnClick/src/main/java/com/backend/globeonclick/configuration/jwt/CorsConfig.java
+++ GlobeOnClick/src/main/java/com/backend/globeonclick/configuration/jwt/CorsConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
@@ -27,5 +29,19 @@ public class CorsConfig {
 
         source.registerCorsConfiguration("/api/**", config);
         return new CorsFilter(source);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
     }
 }
